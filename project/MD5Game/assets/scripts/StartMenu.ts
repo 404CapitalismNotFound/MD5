77e3e23ea14e0dac22dbc8fe4177949890ef4f55
Rerange <78526012+Quandong-Zhang@users.noreply.github.com>
2025-08-01 12:56:34 +0800

    添加各个场景

---
+++ project/MD5Game/assets/scripts/StartMenu.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, director, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('StartMenu')
+export class StartMenu extends Component {
+    onGameStartButtonClick(){
+        director.loadScene("Main")
+    }
+    onRankButtonClick(){
+        //director.loadScene("Main")
+    }
+}
+
+
