094792eae88e4d01e2a69b97e7079aa47bc32b08
zexin-li <www.lixuanyi@foxmial.com>
2025-08-04 14:57:02 +0800

    完善注释

--- project/MD5Game/assets/scripts/Objs/Faction.ts
+++ project/MD5Game/assets/scripts/Objs/Faction.ts
@@ -1,3 +1,10 @@
+/**
+ * @param Su    苏维埃联盟
+ * @param Meng  联盟军 
+ * @param Yuri  尤里阵营
+ * @param FenFeng   焚风阵营
+ */
+
 export enum Faction {//阵营
     Su = "Union",      // 苏联
     Meng = "Alliance",    // 盟军
